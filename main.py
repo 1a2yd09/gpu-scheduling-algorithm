3cf8f492a201d21182f6f96a3a6f7304d475303d
cat <zmfumz@gmail.com>
2020-10-23 21:29:57 +0800

    将显式获取任务名称数组改成从文本当中获取。

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ parser = argparse.ArgumentParser(description='')
 parser.add_argument('-i', '--iteration-times', default=1000, type=int)
 parser.add_argument('-g', '--gpu-num', default=8, type=int)
 parser.add_argument('-n', '--individual-num', default=50, type=int)
-parser.add_argument('-s', '--image', action='store_true')
+parser.add_argument('-c', '--category', default='image', type=str)
 args = parser.parse_args()
-
-image_names = ['alexnet', 'resnet50', 'resnext50', 'seresnet101', 'googlenet', 'vgg16', 'densenet201']
-action_names = ['tsn', 'tsm', 'slowonly', 'slowfast', 'r2plus1d', 'i3d']
 
 
 def main():
-    job_names = image_names if args.image else action_names
+    job_names = []
+    with open(f'./job_name_data/{args.category}.txt', 'r') as f:
+        for job_name in f:
+            job_names.append(job_name.strip())
+
     gpu_num = args.gpu_num
+
     data = get_training_data(job_names, gpu_num)
 
     sequential_execution(job_names, gpu_num, data)
